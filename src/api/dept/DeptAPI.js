07877ac86956202ab972a7b9de9627f82208a8b0
hg_yellow <63451752+jang010505@users.noreply.github.com>
2024-07-09 18:13:10 +0900

    Update DeptAPI.js

--- src/api/dept/DeptAPI.js
+++ src/api/dept/DeptAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const DeptApi = axios.create({
-    baseURL: `http://${process.env.REACT_APP_SERVER_HOST}:8080`
+    baseURL: `http://${process.env.REACT_APP_SERVER_HOST}:80`
 });
 
 export const fetchDepts = async () => {
