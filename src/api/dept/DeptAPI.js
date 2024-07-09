fb120ba259934d2260908c04150b77f15a8e1fbd
hg_yellow <jang010505@gmail.com>
2024-07-09 16:28:23 +0900

    feat: Change server host

--- src/api/dept/DeptAPI.js
+++ src/api/dept/DeptAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const DeptApi = axios.create({
-    baseURL: 'http://127.0.0.1:8080'
+    baseURL: `http://${process.env.REACT_APP_SERVER_HOST}:8080`
 });
 
 export const fetchDepts = async () => {
